13d36293ee6c6250635b6599d02feb457b0d8395
Minh Nguyen <minh260903@gmail.com>
2023-04-14 00:03:47 +0700

    Hoàn thành form gói tập

---
+++ PBL03_DoAnCongNghePhanMem/QLPhongGym/DAL/GoiTapDAL.cs
@@ -0,0 +1,102 @@
+﻿using QLPhongGym.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLPhongGym.DAL
+{
+    class GoiTapDAL
+    {
+        private static GoiTapDAL instance;
+        public static GoiTapDAL Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new GoiTapDAL();
+                return instance;
+            }
+            private set { }
+        }
+        public DataTable TaoBang()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("STT", typeof(int));
+            dt.Columns.Add("Mã gói tập", typeof(int));
+            dt.Columns.Add("Gói tập", typeof(string));
+            dt.Columns.Add("Giá(vnđ)", typeof(double));
+            return dt;
+        }
+        public DataTable GetData_DAL()
+        {
+            DataTable data = new DataTable();
+            int cnt = 1;
+            using (QLPhongGymDB db = new QLPhongGymDB())
+            {
+                data = TaoBang();
+                var str = from p in db.GoiTaps select new { p.IDGT, p.Name, p.Price };
+                foreach (var item in db.GoiTaps)
+                {
+                    data.Rows.Add(cnt++, item.IDGT, item.Name, item.Price);
+                }
+                return data;
+            }
+        }
+        public void DeleteGT_DAL(int mgt)
+        {
+            try
+            {
+                using (QLPhongGymDB db = new QLPhongGymDB())
+                {
+                    GoiTap query = db.GoiTaps.Where(p => p.IDGT == mgt).FirstOrDefault();
+                    db.GoiTaps.Remove(query);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi" + ex.Message);
+            }
+        }
+        public void AddGoiTap_DAL(GoiTap gt)
+        {
+            try
+            {
+                using (QLPhongGymDB db = new QLPhongGymDB())
+                {
+                    db.GoiTaps.Add(gt);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Lỗi: " + e.Message);
+                throw;
+            }
+        }
+        public void UpdateGoiTap_DAL(GoiTap tb)
+        {
+            using (QLPhongGymDB db = new QLPhongGymDB())
+            {
+                var s = db.GoiTaps.FirstOrDefault(p => p.IDGT == tb.IDGT);
+                try
+                {
+                    s.IDGT = tb.IDGT;
+                    s.Name = tb.Name;
+                    s.Price = tb.Price;
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Lỗi" + e.Message);
+                    throw;
+                }
+
+            }
+        }
+    }
+}
--- PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/Admin_FormDanhSachGoiTap.cs
+++ PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/Admin_FormDanhSachGoiTap.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QLPhongGym.BLL;
+using QLPhongGym.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,119 @@ namespace QLPhongGym.GUI
         public Admin_FormDanhSachGoiTap()
         {
             InitializeComponent();
+            LoadDTG();
+        }
+        public void LoadDTG()
+        {
+            dataGridView1.DataSource = GoiTapBLL.Instance.GetData_BLL();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show(string.Format("Bạn có chắc chắn muốn xóa {0} hàng đã chọn?", dataGridView1.SelectedRows.Count),
+                                                      "Xóa dữ liệu",
+                                                      MessageBoxButtons.YesNo,
+                                                      MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    // Xóa các hàng được chọn
+                    foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                    {
+                        if (row.Cells["Mã gói tập"] != null && row.Cells["Mã gói tập"].Value != null)
+                        {
+                            int id = Convert.ToInt32(row.Cells["Mã gói tập"].Value);
+                            GoiTapBLL.Instance.DeleteGT_BLL(id);
+                        }
+
+                    }
+                }
+                Reset();
+                LoadDTG();
+            }
+        }
+        public void Reset()
+        {
+            txt_MGT.Text = "";
+            txt_Price.Text = "";
+            txt_TenGT.Text = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txt_MGT.Text = row.Cells["Mã gói tập"].Value.ToString();
+                txt_TenGT.Text = row.Cells["Gói tập"].Value.ToString();
+                txt_Price.Text = row.Cells["Giá(vnđ)"].Value.ToString();
+            }
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            if (txt_MGT.Text == "")
+            {
+                GoiTap tb = new GoiTap();
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.Cells["Gói tập"].Value != null)
+                    {
+                        if (txt_TenGT.Text == row.Cells["Gói tập"].Value.ToString())
+                        {
+                            MessageBox.Show("Gói tập đã tồn tại trong danh sách, vui lòng tạo gói khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                }
+                if(txt_TenGT.Text == "")
+                {
+                    MessageBox.Show("Gói tập chưa có tên", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!int.TryParse(txt_Price.Text, out int price) || price <= 0)
+                {
+                    MessageBox.Show("Giá gói tập không hợp lệ, vui lòng nhập lại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                tb.Name = txt_TenGT.Text;
+                tb.Price = Convert.ToDouble(txt_Price.Text);
+                GoiTapBLL.Instance.AddGoiTap_BLL(tb);
+                MessageBox.Show("Đã thêm thành công!!!");
+                Reset();
+            }
+            LoadDTG();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            GoiTap tb = new GoiTap();
+            if (txt_MGT.Text != "")
+            {
+                if (txt_TenGT.Text == "")
+                {
+                    MessageBox.Show(" Cần nhập tên thiết bị", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!int.TryParse(txt_Price.Text, out int price) || price <= 0)
+                {
+                    MessageBox.Show("Giá thiết bị không hợp lệ, vui lòng nhập lại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                tb.IDGT = Convert.ToInt32(txt_MGT.Text);
+                tb.Price = Convert.ToDouble(txt_Price.Text);
+                tb.Name = txt_TenGT.Text;
+                GoiTapBLL.Instance.UpdateGoiTap_BLL(tb);
+                MessageBox.Show("Đã sửa thành công!!!");
+                Reset();
+            }
+            LoadDTG();
         }
     }
 }
