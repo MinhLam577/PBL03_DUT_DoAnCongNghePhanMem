2af575c75ecd408e75e792f758c3d27930cbdad5
Lâm Nhật Minh <minh32405@gmail.com>
2023-05-23 22:26:43 +0700

    Cật nhật admin_form

--- PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/Admin_FormMain.Designer.cs
+++ PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/Admin_FormMain.Designer.cs
@@ -33,16 +33,9 @@
             this.panel_hlv = new System.Windows.Forms.Panel();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.Menu_Panel = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
-            this.panel_tk = new System.Windows.Forms.Panel();
             this.button16 = new System.Windows.Forms.Button();
             this.button_ThietBi = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -54,8 +47,6 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel_hlv.SuspendLayout();
-            this.Menu_Panel.SuspendLayout();
-            this.panel_tk.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -86,7 +77,7 @@
             this.panel_hlv.Controls.Add(this.button8);
             this.panel_hlv.Controls.Add(this.button9);
             this.panel_hlv.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_hlv.Location = new System.Drawing.Point(0, 518);
+            this.panel_hlv.Location = new System.Drawing.Point(0, 393);
             this.panel_hlv.Name = "panel_hlv";
             this.panel_hlv.Size = new System.Drawing.Size(219, 200);
             this.panel_hlv.TabIndex = 4;
@@ -124,22 +115,6 @@
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
-            // button12
-            // 
-            this.button12.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button12.FlatAppearance.BorderSize = 0;
-            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button12.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button12.Location = new System.Drawing.Point(0, 110);
-            this.button12.Name = "button12";
-            this.button12.Padding = new System.Windows.Forms.Padding(39, 0, 0, 0);
-            this.button12.Size = new System.Drawing.Size(219, 54);
-            this.button12.TabIndex = 3;
-            this.button12.Text = "button12";
-            this.button12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button12.UseVisualStyleBackColor = true;
-            // 
             // button13
             // 
             this.button13.BackColor = System.Drawing.Color.Black;
@@ -148,7 +123,7 @@
             this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button13.Location = new System.Drawing.Point(0, 902);
+            this.button13.Location = new System.Drawing.Point(0, 777);
             this.button13.Name = "button13";
             this.button13.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
             this.button13.Size = new System.Drawing.Size(219, 91);
@@ -156,7 +131,6 @@
             this.button13.Text = "Thống kê";
             this.button13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button13.UseVisualStyleBackColor = false;
-            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button10
             // 
@@ -166,7 +140,7 @@
             this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button10.Location = new System.Drawing.Point(0, 422);
+            this.button10.Location = new System.Drawing.Point(0, 297);
             this.button10.Name = "button10";
             this.button10.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
             this.button10.Size = new System.Drawing.Size(219, 96);
@@ -176,86 +150,6 @@
             this.button10.UseVisualStyleBackColor = false;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
-            // Menu_Panel
-            // 
-            this.Menu_Panel.BackColor = System.Drawing.Color.PeachPuff;
-            this.Menu_Panel.Controls.Add(this.button4);
-            this.Menu_Panel.Controls.Add(this.button2);
-            this.Menu_Panel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Menu_Panel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Menu_Panel.Location = new System.Drawing.Point(0, 297);
-            this.Menu_Panel.Name = "Menu_Panel";
-            this.Menu_Panel.Size = new System.Drawing.Size(219, 125);
-            this.Menu_Panel.TabIndex = 2;
-            // 
-            // button4
-            // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button4.Location = new System.Drawing.Point(0, 55);
-            this.button4.Name = "button4";
-            this.button4.Padding = new System.Windows.Forms.Padding(39, 0, 0, 0);
-            this.button4.Size = new System.Drawing.Size(219, 55);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "Lịch tập cùng huấn luyện viên";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(0, 0);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(39, 0, 0, 0);
-            this.button2.Size = new System.Drawing.Size(219, 55);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Thông tin khách hàng";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button6
-            // 
-            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button6.Location = new System.Drawing.Point(0, 55);
-            this.button6.Name = "button6";
-            this.button6.Padding = new System.Windows.Forms.Padding(39, 0, 0, 0);
-            this.button6.Size = new System.Drawing.Size(219, 55);
-            this.button6.TabIndex = 2;
-            this.button6.Text = "Doanh thu";
-            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            this.button11.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button11.FlatAppearance.BorderSize = 0;
-            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button11.Location = new System.Drawing.Point(0, 0);
-            this.button11.Name = "button11";
-            this.button11.Padding = new System.Windows.Forms.Padding(39, 0, 0, 0);
-            this.button11.Size = new System.Drawing.Size(219, 55);
-            this.button11.TabIndex = 0;
-            this.button11.Text = "Khách hàng đăng kí";
-            this.button11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button11.UseVisualStyleBackColor = true;
-            // 
             // button14
             // 
             this.button14.BackColor = System.Drawing.Color.Black;
@@ -264,7 +158,7 @@
             this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button14.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button14.Location = new System.Drawing.Point(0, 1200);
+            this.button14.Location = new System.Drawing.Point(0, 868);
             this.button14.Name = "button14";
             this.button14.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
             this.button14.Size = new System.Drawing.Size(219, 83);
@@ -274,18 +168,6 @@
             this.button14.UseVisualStyleBackColor = false;
             this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
-            // panel_tk
-            // 
-            this.panel_tk.BackColor = System.Drawing.Color.PeachPuff;
-            this.panel_tk.Controls.Add(this.button12);
-            this.panel_tk.Controls.Add(this.button6);
-            this.panel_tk.Controls.Add(this.button11);
-            this.panel_tk.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_tk.Location = new System.Drawing.Point(0, 993);
-            this.panel_tk.Name = "panel_tk";
-            this.panel_tk.Size = new System.Drawing.Size(219, 207);
-            this.panel_tk.TabIndex = 9;
-            // 
             // button16
             // 
             this.button16.BackColor = System.Drawing.Color.Black;
@@ -294,7 +176,7 @@
             this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button16.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button16.Location = new System.Drawing.Point(0, 809);
+            this.button16.Location = new System.Drawing.Point(0, 684);
             this.button16.Name = "button16";
             this.button16.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
             this.button16.Size = new System.Drawing.Size(219, 93);
@@ -312,7 +194,7 @@
             this.button_ThietBi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_ThietBi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_ThietBi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button_ThietBi.Location = new System.Drawing.Point(0, 718);
+            this.button_ThietBi.Location = new System.Drawing.Point(0, 593);
             this.button_ThietBi.Name = "button_ThietBi";
             this.button_ThietBi.Padding = new System.Windows.Forms.Padding(11, 0, 0, 0);
             this.button_ThietBi.Size = new System.Drawing.Size(219, 91);
@@ -327,13 +209,11 @@
             this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel1.Controls.Add(this.button14);
-            this.panel1.Controls.Add(this.panel_tk);
             this.panel1.Controls.Add(this.button13);
             this.panel1.Controls.Add(this.button16);
             this.panel1.Controls.Add(this.button_ThietBi);
             this.panel1.Controls.Add(this.panel_hlv);
             this.panel1.Controls.Add(this.button10);
-            this.panel1.Controls.Add(this.Menu_Panel);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
@@ -407,6 +287,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(55, 45);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // panel3
             // 
@@ -441,8 +322,6 @@
             this.Text = "Admin_FormMain";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel_hlv.ResumeLayout(false);
-            this.Menu_Panel.ResumeLayout(false);
-            this.panel_tk.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
@@ -459,16 +338,9 @@
         private System.Windows.Forms.Panel panel_hlv;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Button button13;
         private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Panel Menu_Panel;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button14;
-        private System.Windows.Forms.Panel panel_tk;
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button button_ThietBi;
         private System.Windows.Forms.Button button1;
