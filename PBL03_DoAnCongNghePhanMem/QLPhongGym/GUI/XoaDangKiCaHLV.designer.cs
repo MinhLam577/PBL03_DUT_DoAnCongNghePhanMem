9337ca9e330ce101980f38df2c0cc05179a22991
Lâm Nhật Minh <minh32405@gmail.com>
2023-06-02 11:29:07 +0700

    Cật nhật sơ form xếp lịch hlv

--- PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/XoaDangKiCaHLV.designer.cs
+++ PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/XoaDangKiCaHLV.designer.cs
@@ -30,20 +30,20 @@
         {
             this.btnXoa = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnNgaylamviec = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.ngaylamviec = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.cbbCaLam = new System.Windows.Forms.ComboBox();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(12, 228);
+            this.btnXoa.Location = new System.Drawing.Point(12, 212);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(100, 23);
+            this.btnXoa.Size = new System.Drawing.Size(100, 50);
             this.btnXoa.TabIndex = 11;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = true;
@@ -51,29 +51,23 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(133, 198);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(625, 213);
+            this.dataGridView1.Size = new System.Drawing.Size(655, 240);
             this.dataGridView1.TabIndex = 15;
-            // 
-            // btnNgaylamviec
-            // 
-            this.btnNgaylamviec.Location = new System.Drawing.Point(192, 12);
-            this.btnNgaylamviec.Name = "btnNgaylamviec";
-            this.btnNgaylamviec.Size = new System.Drawing.Size(117, 23);
-            this.btnNgaylamviec.TabIndex = 44;
-            this.btnNgaylamviec.Text = "Ngày Làm";
-            this.btnNgaylamviec.UseVisualStyleBackColor = true;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(594, 20);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(598, 13);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.Size = new System.Drawing.Size(93, 25);
             this.label8.TabIndex = 43;
             this.label8.Text = "Hôm Nay";
             // 
@@ -91,9 +85,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(223, 109);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 101);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.Size = new System.Drawing.Size(130, 25);
             this.label1.TabIndex = 48;
             this.label1.Text = "Ca Làm Việc ";
             // 
@@ -107,18 +102,28 @@
             "Ca 4"});
             this.cbbCaLam.Location = new System.Drawing.Point(386, 101);
             this.cbbCaLam.Name = "cbbCaLam";
-            this.cbbCaLam.Size = new System.Drawing.Size(121, 24);
+            this.cbbCaLam.Size = new System.Drawing.Size(175, 24);
             this.cbbCaLam.TabIndex = 47;
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(12, 297);
+            this.btnCancel.Location = new System.Drawing.Point(12, 347);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(100, 23);
+            this.btnCancel.Size = new System.Drawing.Size(100, 54);
             this.btnCancel.TabIndex = 49;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 25);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Ngày lảm việc";
             // 
             // XoaDangKiCaHLV
             // 
@@ -126,10 +131,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbbCaLam);
-            this.Controls.Add(this.btnNgaylamviec);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.ngaylamviec);
             this.Controls.Add(this.dataGridView1);
@@ -147,11 +152,11 @@
 
         private System.Windows.Forms.Button btnXoa;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnNgaylamviec;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.DateTimePicker ngaylamviec;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbbCaLam;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
     }
 }
