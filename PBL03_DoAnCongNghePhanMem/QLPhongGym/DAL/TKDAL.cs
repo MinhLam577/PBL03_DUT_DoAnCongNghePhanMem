07b975ecaa2abe60bc325deef16db106d20d7b01
Lâm Nhật Minh <minh32405@gmail.com>
2023-06-08 04:24:46 +0700

    Ko lỗi

--- PBL03_DoAnCongNghePhanMem/QLPhongGym/DAL/TKDAL.cs
+++ PBL03_DoAnCongNghePhanMem/QLPhongGym/DAL/TKDAL.cs
@@ -27,7 +27,8 @@ namespace DAL
         }
         public List<TK> LoadAllTK()
         {
-            return db.TKs.ToList();
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.ToList();
         }
         public bool checkTkMk(string ac)
         {
@@ -44,45 +45,64 @@ namespace DAL
         }
         public bool CheckMKTKExist(string tk, string mk)
         {
-            return LoadAllTK().Any(t => (t.TenTK.Equals(tk) && t.MatkhauTK.Equals(Eramake.eCryptography.Encrypt(mk))) || (t.TenTK.Equals(tk) && t.MatkhauTK.Equals(mk)));
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.ToList().Any(t => (t.TenTK.Equals(tk) && t.MatkhauTK.Equals(Eramake.eCryptography.Encrypt(mk))) || (t.TenTK.Equals(tk) && t.MatkhauTK.Equals(mk)));
         }
         public bool CheckTenTKExist(string tentk)
         {
-            return LoadAllTK().Any(t => t.TenTK.Equals(tentk));
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.ToList().Any(t => t.TenTK.Equals(tentk));
         }
         public string GetUserByMaQuyen(int IDQuyen)
         {
-            return LoadAllTK().Where(t => t.IDQuyen.Equals(IDQuyen)).Select(t => t.PhanQuyen.TenQuyen).FirstOrDefault();
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.ToList().Where(t => t.IDQuyen.Equals(IDQuyen)).Select(t => t.PhanQuyen.TenQuyen).FirstOrDefault();
         }
         public int GetIDQuyen(string tentk) {
-            return LoadAllTK().Where(t => t.TenTK.Equals(tentk)).Select(t => t.IDQuyen).FirstOrDefault();
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.ToList().Where(t => t.TenTK.Equals(tentk)).Select(t => t.IDQuyen).FirstOrDefault();
         }
         public int AddTK(TK acc)
         {
-            db.TKs.Add(acc);
-            return db.SaveChanges();
+            using (QLPhongGymDB db = new QLPhongGymDB())
+            {
+                db.TKs.Add(acc);
+                return db.SaveChanges();
+            }
+                
         }
         public void DeleteTK(TK acc)
         {
-            db.TKs.Remove(acc);
-            db.SaveChanges();
+            using (QLPhongGymDB db = new QLPhongGymDB())
+            {
+                db.Entry(acc).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+                
         }
         public int UpdateTK(TK acc)
         {
-            db.Entry(acc).State = System.Data.Entity.EntityState.Modified;
-            return db.SaveChanges();
+            using (QLPhongGymDB db = new QLPhongGymDB())
+            {
+                db.Entry(acc).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges();
+            }
+                
         }
         public TK GetTKByUserID(int ID)
         {
-            return db.TKs.FirstOrDefault(tk => (int)tk.IDUser == ID);
+            using(QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.FirstOrDefault(tk => (int)tk.IDUser == ID);
         }
         public TK GetTKByTenTK(string TenTK)
         {
-            return db.TKs.FirstOrDefault(tk => tk.TenTK.Equals(TenTK));
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.FirstOrDefault(tk => tk.TenTK.Equals(TenTK));
         }
         public TK GetTKByMK(string MKTK)
         {
-            return db.TKs.FirstOrDefault(tk => tk.MatkhauTK.Equals(Eramake.eCryptography.Encrypt(MKTK)));
+            using (QLPhongGymDB db = new QLPhongGymDB())
+                return db.TKs.FirstOrDefault(tk => tk.MatkhauTK.Equals(Eramake.eCryptography.Encrypt(MKTK)));
         }
 
     }
