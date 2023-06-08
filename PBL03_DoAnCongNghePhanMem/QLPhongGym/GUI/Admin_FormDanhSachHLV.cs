7f524db4492b507c0d2b79acf668d4cf0c18bc48
Lâm Nhật Minh <minh32405@gmail.com>
2023-06-08 17:28:01 +0700

    Update Admin_FormDanhSachHLV.cs

--- PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/Admin_FormDanhSachHLV.cs
+++ PBL03_DoAnCongNghePhanMem/QLPhongGym/GUI/Admin_FormDanhSachHLV.cs
@@ -37,32 +37,41 @@ namespace QLPhongGym.GUI
                 {
                     if (dataGridView1.SelectedCells.Count > 0)
                     {
-                        DataGridViewCell selectedCell = dataGridView1.SelectedCells[0];
-                        if (selectedCell.OwningRow.Cells[1].Value != null)
+                        bool check = true;
+                        foreach(DataGridViewCell i in dataGridView1.SelectedCells)
                         {
-                            int idHLV = Convert.ToInt32(selectedCell.OwningRow.Cells[1].Value.ToString().Trim());
+                            if (i.OwningRow.Cells[1].Value != null)
+                            {
+                                int idHLV = Convert.ToInt32(i.OwningRow.Cells[1].Value.ToString().Trim());
 
-                            //Xóa tài khoản hlv
-                            if (TKHLV_BLL.Instance.FindListTKHLVByIDHLV(idHLV) != null)
-                                foreach (string TenTK in TKHLV_BLL.Instance.FindListTKHLVByIDHLV(idHLV))
-                                    TKBLL.Instance.DeleteTK(TKBLL.Instance.GetTKByTenTK(TenTK));
+                                //Xóa tài khoản hlv
+                                if (TKHLV_BLL.Instance.FindListTKHLVByIDHLV(idHLV) != null)
+                                    foreach (string TenTK in TKHLV_BLL.Instance.FindListTKHLVByIDHLV(idHLV))
+                                        TKBLL.Instance.DeleteTK(TKBLL.Instance.GetTKByTenTK(TenTK));
 
-                            //Xóa lịch làm việc hlv
-                            if (DangKiLichLamViecBAL.getInStance.GetListLichLamViecByIDHLV(idHLV) != null)
-                                foreach (LichLamViecTrongTuan llv in DangKiLichLamViecBAL.getInStance.GetListLichLamViecByIDHLV(idHLV))
-                                    DangKiLichLamViecBAL.getInStance.DeleteLichLamViec(llv);
+                                //Xóa lịch làm việc hlv
+                                if (DangKiLichLamViecBAL.getInStance.GetListLichLamViecByIDHLV(idHLV) != null)
+                                    foreach (LichLamViecTrongTuan llv in DangKiLichLamViecBAL.getInStance.GetListLichLamViecByIDHLV(idHLV))
+                                        DangKiLichLamViecBAL.getInStance.DeleteLichLamViec(llv);
 
-                            //Xóa lịch thuê hlv
-                            if (LichThueBLL.Instance.GetLichThueByIDHLV(idHLV) != null)
-                                foreach (LichThueHLV lt in LichThueBLL.Instance.GetLichThueByIDHLV(idHLV))
-                                    LichThueBLL.Instance.DeleteLichThue(lt.IDLT);
+                                //Xóa lịch thuê hlv
+                                if (LichThueBLL.Instance.GetLichThueByIDHLV(idHLV) != null)
+                                    foreach (LichThueHLV lt in LichThueBLL.Instance.GetLichThueByIDHLV(idHLV))
+                                        LichThueBLL.Instance.DeleteLichThue(lt.IDLT);
 
-                            //Xóa huấn luyện viên
-                            if (QLHLVBLL.getInstance.Delete(idHLV))
+                                //Xóa huấn luyện viên
+                                if (QLHLVBLL.getInstance.Delete(idHLV))
+                                {
+                                    check = false;
+                                    break;
+                                }
+                            }
+                            if(check)
                             {
                                 MessageBox.Show("Xóa Thành Công");
                                 CapNhatListHLV();
                             }
+                            else MessageBox.Show("Xóa thất bại");
                         }
                     }
                 }
